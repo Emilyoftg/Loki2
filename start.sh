ac6927c1f62c8f32f6ba18621d5899cba8af7646
Emily <71280699+Emilyoftg@users.noreply.github.com>
2022-10-30 19:31:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sources-cods/EvaMaria.git /EvaMaria
+  git clone https://github.com/Emilyoftg/Loki2.git /Loki2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Loki2
 fi
-cd /EvaMaria
+cd /Loki2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
